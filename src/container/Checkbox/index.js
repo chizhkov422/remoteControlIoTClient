17f072ac4fe120622df37973bbc9ac8155d49a77
Alexandr Chizhkov <chizhkov422@gmail.com>
2019-04-14 20:10:14 +0300

    Added checbox && server state to checkbox

---
+++ src/container/Checkbox/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Checkbox from '../../components/Checkbox'
+
+class CheckboxCont extends Component {
+  state = {
+    checkboxState: false,
+  }
+  componentDidMount() {
+    axios.get('https://raspberry-web-app-server.herokuapp.com/api/states/getState/checkbox')
+      .then(response => {
+        this.setState({ checkboxState: Boolean(response.data.data.stateValue) });
+      });
+  }
+  changeCheckboxState = (e) => {
+    const data = JSON.stringify({
+      stateName: 'checkbox',
+      stateValue: e.target.checked ? 1 : 0
+    });
+
+    axios.post('https://raspberry-web-app-server.herokuapp.com/api/states/update', {
+      state: data
+    });
+
+    this.setState({ checkboxState: e.target.checked });
+  }
+  render() {
+    return (
+      <>
+        <Checkbox changeHandlerForCheckbox={this.changeCheckboxState} chackboxState={this.state.checkboxState} />
+      </>
+    );
+  }
+}
+
+export default CheckboxCont;
