2257e4913ae1a8f813feff3557906296425fd8ad
Alexandr Chizhkov <chizhkov422@gmail.com>
2019-04-30 19:59:15 +0300

    Removed comment

--- src/container/App.js
+++ src/container/App.js
@@ -10,15 +10,15 @@ class App extends Component {
     minTemp: 0,
     maxTemp: 0,
   }
-  // componentDidMount() {
-  //   axios.get('https://remote-control-iot-server.herokuapp.com/api/states/getState/temperature')
-  //     .then(response => {
-  //       this.setState({ currentMode: response.data.data.currentMode });
-  //       this.setState({ manualTemp: response.data.data.manualTemp });
-  //       this.setState({ minTemp: response.data.data.minTemp });
-  //       this.setState({ maxTemp: response.data.data.maxTemp });
-  //     });
-  // }
+  componentDidMount() {
+    axios.get('https://remote-control-iot-server.herokuapp.com/api/states/getState/temperature')
+      .then(response => {
+        this.setState({ currentMode: response.data.data.currentMode });
+        this.setState({ manualTemp: response.data.data.manualTemp });
+        this.setState({ minTemp: response.data.data.minTemp });
+        this.setState({ maxTemp: response.data.data.maxTemp });
+      });
+  }
   changeMode = (mode) => {
     this.setState({ currentMode: mode });
   }
